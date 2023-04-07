500754b6f4d15d53ab0166333786cb2e02df2fe6
sowmya <sowmyakv22@gmail.com>
2023-04-08 01:56:24 +0530

    Initial setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app1 = express();
+
+app1.set("view engine","ejs");
+app1.use( express.static( "public" ) );
+app1.use(express.static(__dirname + '/public'));
+
+app1.get("/",(req,res)=>{
+    res.render("home")
+})
+app1.get("/SignUp",(req,res)=>{
+    res.render("SignUp")
+})
+app1.get("/Login",(req,res)=>{
+    res.render("Login")
+})
+app1.get("/home",(req,res)=>{
+    res.render("Expenses")
+})
+app1.get("/analytics",(req,res)=>{
+    res.render("Analytics")
+})
+app1.get("/password",(req,res)=>{
+    res.render("password")
+})
+
+app1.listen(3000)
